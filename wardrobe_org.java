71000a7ca6c6344d38e51129b2f1fda26d68fbd0
sanjana <sanajanakovuru@gmail.com>
2026-06-17 00:02:51 +0530

    Add wardrobe organization recommendation program
    This program evaluates clothing items based on user input regarding wear, fit, damage, and preference, providing a recommendation to keep, donate, or throw away the item.

---
+++ wardrobe_org.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class wardrobe_org {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String rec;
+        String score = 0;
+
+        System.out.println("Have you worn it in the last 1 year? (y/n): ");
+        String worn = sc.nextLine();
+
+        System.out.println("Does it fit well? (y/n): ") ;
+        String fit = sc.nextLine();
+
+        System.out.println("Is it damaged? (y/n): ");
+        String damage = sc.nextLine();
+
+        System.out.println("Do you like wearing it? (y/n): ");
+        String like = sc.nextLine();
+
+        if(damage.equalsIgnoreCase("y")){
+            rec = "THROW AWAY";
+        }
+
+        else if(fit.equalsIgnoreCase("n")){
+            rec = "DONATE";
+        }
+
+        else if(worn.equalsIgnoreCase("n") && like.equalsIgnoreCase("n")){
+            rec = "DONATE";
+        }
+
+        else{
+            rec = "KEEP";
+        }
+
+        System.out.println("Recommendation: " + rec);
+        
+        if(worn.equalsIgnoreCase("yes"))
+            score += 3;
+
+        if(fit.equalsIgnoreCase("yes"))
+            score += 3;
+
+        if(like.equalsIgnoreCase("yes"))
+            score += 3;
+        
+        sc.close();
+
+    }
+}
