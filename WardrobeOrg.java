a5647b32a7d5f8ba9cb7627b90c3ed3e74f6d133
sanjana <sanajanakovuru@gmail.com>
2026-06-17 20:42:11 +0530

    Update and rename wardrobe_org.java to WardrobeOrg.java

--- WardrobeOrg.java
+++ WardrobeOrg.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class wardrobe_org {
+public class WardrobeOrg {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String rec;
